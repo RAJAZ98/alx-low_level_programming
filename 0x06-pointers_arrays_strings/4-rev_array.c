edfa807a090325d8e528215e3917802740b628cb
rajaa zekri <rajae.zekri.98@gmail.com>
2023-05-05 18:02:31 -0700

    4. I am a kind of paranoid in reverse. I suspect people of plotting to make me happy

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- *  reverse_array - a function that reverses the content of an array of integers.
- *
+ * reverse_array - a function that reverses the content of an array of integers.
  * @a: input value
  * @n: input value
  *
